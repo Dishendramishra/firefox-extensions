40fbbeb21b6197112689455b7c51e9902ce64664
Dishendra <dishendramishra@gmail.com>
2019-01-28 02:20:38 +0530

    some updates

---
+++ reading-page-cleaner/reading-page-cleaner.js
@@ -0,0 +1,5 @@
+try{
+	document.getElementsByClassName('credits reader-credits')[0].remove();
+	document.getElementsByClassName('meta-data')[0].remove();
+	document.getElementsByClassName('domain reader-domain')[0].remove();	
+}catch(err){}
